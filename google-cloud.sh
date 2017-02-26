1ecef01e33d743cbf02006e97d36b82a1839fc74
Kasper Marstal <kaspermarstal@gmail.com>
2017-02-26 15:20:54 +0000

    ENH: Fix google-cloud.sh syntax errors

--- google-cloud.sh
+++ google-cloud.sh
@@ -33,25 +33,25 @@ if [ $1="--create-gpu-instance" ]; then
     fi'
 fi
 
-if [ $1="--clone-source"]
+if [ $1="--clone-source"]; then
   git clone https://github.com/kaspermarstal/brainnet ~/brainnet
 fi
 
-if [ $1="--download-data"]
+if [ $1="--download-data" ]; then
   if ![ -d "~/downloads" ]; then
-  mkdir "~/downloads"
+    mkdir "~/downloads"
   fi
   curl -OL ftp://ftp.nrg.wustl.edu/data/oasis_cross-sectional_disc1.tar.gz -C download
 fi
 
-if [ $1="--extract-data"]
+if [ $1="--extract-data"]; then
   if ![ -d "~/data" ]; then
-  mkdir "~/data"
+    mkdir "~/data"
   fi
   tar -xcf ~/downloads/oasis_cross-sectional_disc1.tar.gz ~/data
 fi
 
-if [ $1="--setup-python-env"]
+if [ $1="--setup-python-env"]; then
   sudo apt-get install virtualenv
   virtualenv venv
   pip install tensorflow keras SimpleITK
