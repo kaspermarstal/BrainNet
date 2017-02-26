d5b20f248823c4440ee0643daaf964d272de9e35
Kasper Marstal <kaspermarstal@gmail.com>
2017-02-26 18:50:30 +0100

    ENH: Reduce number of cores for GPU instance

--- google-cloud.sh
+++ google-cloud.sh
@@ -15,10 +15,10 @@ fi
 if [ $1="--create-gpu-instance" ]; then
   gcloud beta compute instances create brainnet-gpu \
     --zone europe-west1-b \
-    --machine-type n1-standard-4 \
+    --machine-type n1-highmem-2 \
     --image-project ubuntu-os-cloud \
     --image-family ubuntu-1604-lts \
-    --boot-disk-device-name=brainnet \
+    --boot-disk-device-name=brainnet-gpu \
     --boot-disk-type=pd-standard \
     --boot-disk-size=64GB \
     --accelerator type=nvidia-tesla-k80,count=1 \
