ebdf1152a075f28fa48c7c0ab4af5f3aab36b967
Kasper Marstal <kaspermarstal@gmail.com>
2017-02-26 23:25:07 +0100

    BUG: Fix shell script if

--- google-cloud.sh
+++ google-cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1="--create-cpu-instance" ]; then
+if [[ $1 == "--create-cpu-instance" ]]; then
   gcloud beta compute instances create brainnet-cpu \
     --zone europe-west1-b \
     --machine-type n1-highcpu-16 \
@@ -12,7 +12,7 @@ if [ $1="--create-cpu-instance" ]; then
     --maintenance-policy TERMINATE --restart-on-failure
 fi
 
-if [ $1="--create-gpu-instance" ]; then
+if [[ $1 == "--create-gpu-instance" ]]; then
   gcloud beta compute instances create brainnet-gpu \
     --zone europe-west1-b \
     --machine-type n1-highmem-2 \
