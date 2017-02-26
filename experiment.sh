a8d2c700e36d1ae934081cf1037d97b95e426323
Kasper Marstal <kaspermarstal@gmail.com>
2017-02-26 19:44:07 +0100

    ENH: Add BrainNet as root folder

--- experiment.sh
+++ experiment.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-if [[ $1 == "--clone-source" ]]; then
-  git clone https://github.com/kaspermarstal/brainnet ~/brainnet
-  exit
-fi
-
 if [[ $1 == "--download-data" ]]; then
-  if [[ -d ~/downloads ]]; then
-    rm -fr ~/downloads
-    mkdir ~/downloads
+  if [[ -d ~/BrainNet/downloads ]]; then
+    rm -fr ~/BrainNet/downloads
   fi
-  cd ~/downloads
-  curl -OL ftp://ftp.nrg.wustl.edu/data/oasis_cross-sectional_disc{1,2,3,4,5,6,7,8,9,10,11,12}.tar.gz
+  mkdir -p ~/BrainNet/downloads
+  cd ~/BrainNet/downloads
+  curl -OL ftp://ftp.nrg.wustl.edu/data/oasis_cross-sectional_disc1.tar.gz
 fi
 
 if [[ $1 == "--extract-data" ]]; then
-  if [[ -d ~/data ]]; then
-    rm -fr ~/data
-    mkdir ~/data
+  if [[ -d ~/BrainNet/data ]]; then
+    rm -fr ~/BrainNet/data
   fi
+  mkdir -p ~/BrainNet/data
   for filename in ~/downloads/*.tar.gz
   do
     tar zxf $filename -C ~/data
@@ -26,21 +21,33 @@ if [[ $1 == "--extract-data" ]]; then
 fi
 
 if [[ $1 == "--install-venv" ]]; then
-  if [[ -d ~/venv ]]; then
-    rm -fr ~/venv
+  if [[ -d ~/BrainNet/venv ]]; then
+    rm -fr ~/BrainNet/venv
   fi
   sudo apt-get install -y virtualenv gcc python-dev
-  virtualenv ~/venv
-  source ~/venv/bin/activate
+  virtualenv ~/BrainNet/venv
+  source ~/BrainNet/venv/bin/activate
   pip install keras SimpleITK numpy sklearn scikit-image tensorflow
 fi
 
 if [[ $1 == "--install-tensorflow-gpu" ]]; then
-  source ~/venv/bin/activate
+  source ~/BrainNet/venv/bin/activate
   pip install --upgrade tensorflow-gpu
 fi
 
-if [[ $1 == "--run" ]]; then
-  source ~/venv/bin/activate
-  python ~/brainnet/train_2d.py
+if [[ $1 == "--run-inception-v4" ]]; then
+  source ~/BrainNet/venv/bin/activate
+  python ~/BrainNet/BrainNet/train_inception_v4.py --data-dir=$HOME/data
+fi
+
+if [[ $1 == "--run-inception-resnet-v4" ]]; then
+  source ~/BrainNet/venv/bin/activate
+  python ~/BrainNet/BrainNet/train_inception_resnet_v4.py --data-dir=$HOME/BrainNet/data
+fi
+
+if [ -z $1 ]; then
+   ./experiment.sh --download-data
+   ./experiment.sh --extract-data
+   ./experiment.sh --install-venv
+   ./experiment.sh --run-inception-v4
 fi
